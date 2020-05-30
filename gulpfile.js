526d5cb992fca7ce1618274fe9d433eeb9cb6b94
DanielApril <danielbfmv@yandex.ru>
2020-05-31 01:49:07 +0300

    restore pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,229 @@
-// Initialize modules
-// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
-const { src, dest, watch, series, parallel } = require('gulp');
-// Importing all the Gulp-related packages we want to use
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const rev = require("gulp-rev");
+const revRewrite = require("gulp-rev-rewrite");
+const babel = require("gulp-babel");
+const del = require("del");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const pump = require("pump");
+const cleanCSS = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
 const gulpStylelint = require('gulp-stylelint');
-var replace = require('gulp-replace');
+sass.compiler = require("node-sass");
 
+// style paths
+var sass_src = "./src/sass/main.scss",
+  sass_files = "./src/sass/*.scss",
+  img_src = "./src/assets/**/",
+  html_src = "./src/**/*.html",
+  js_src = "./src/**/*.js",
+  dist = "./dist",
+  html_dest = "./dist/**/*.html",
+  assets = "./dist/assets",
+  build = "./dist/build/",
+  temp = "./dist/build/temp/",
+  js_temp = "./dist/build/temp/js",
+  css_temp = "./dist/build/temp/css",
+  jquery = "node_modules/jquery/dist/jquery.min.js",
+  popperjs = "node_modules/popper.js/dist/umd/popper.min.js",
+  bootstrap = "node_modules/bootstrap/dist/js/bootstrap.min.js";
 
-// File paths
-const files = {
-    stylesPath: 'src/scss/**/*.{sass,scss,css}',
-	scriptsPath: 'src/js/**/*.js',
-	distPath: 'dist'
-}
+// hashing task
+gulp.task("hash", function() {
+  return gulp
+    .src([temp + "**/*.js", temp + "**/*.css"])
+    .pipe(rev())
+    .pipe(gulp.dest(dist))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest(assets));
+});
 
-// Sass task: compiles the style.scss file into style.css
-function scssTask(){
-	return src(files.stylesPath)
-		.pipe(gulpStylelint({
-			fix: true
-		}))
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass()) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-		.pipe(dest('distPath')
-    ); // put final CSS in dist folder
-}
-
-// JS task: concatenates and uglifies JS files to script.js
-function jsTask(){
-    return src([
-        files.scriptsPath
-        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
-        ])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-		.pipe(dest('distPath')
-    );
-}
-
-// Cachebust
-function cacheBustTask(){
-    var cbString = new Date().getTime();
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
-}
-
-// Watch task: watch SCSS and JS files for changes
-// If any change, run scss and js tasks simultaneously
-function watchTask(){
-    watch([files.stylesPath, files.scriptsPath],
-        {interval: 1000, usePolling: true}, //Makes docker work
-        series(
-            parallel(scssTask, jsTask),
-            cacheBustTask
-        )
-    );
-}
-
-// Export the default Gulp task so it can be run
-// Runs the scss and js tasks simultaneously
-// then runs cacheBust, then watch task
-exports.default = series(
-    parallel(scssTask, jsTask),
-    cacheBustTask,
-    watchTask
+// cleaning dist folder
+gulp.task(
+  "clean-build",
+  gulp.series("hash", done => {
+    return del([build]);
+    done();
+  })
 );
+
+// inject hashed files to html
+gulp.task(
+  "update",
+  gulp.series("clean-build", function(done) {
+    const manifest = gulp.src(assets + "/rev-manifest.json");
+    return gulp
+      .src(html_dest)
+      .pipe(revRewrite({ manifest }))
+      .pipe(gulp.dest(dist));
+    done();
+  })
+);
+
+// Compile sass into CSS
+gulp.task("build-sass", () => {
+  return gulp
+	.src(sass_src)
+	.pipe(gulpStylelint({
+		fix: true
+	}))
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("style.css"))
+    .pipe(sourcemaps.write())
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest(css_temp))
+    .pipe(browserSync.stream());
+});
+
+// bundle dependencies js
+gulp.task("vendor-js", done => {
+  return gulp
+    .src([jquery, popperjs, bootstrap])
+    .pipe(concat("vendor-bundle.js"))
+    .pipe(gulp.dest(build));
+  done();
+});
+
+// babel build task
+gulp.task("build-js", () => {
+  return gulp
+    .src(js_src)
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest(build));
+});
+
+// bundle all js
+gulp.task(
+  "bundle-js",
+  gulp.series(gulp.parallel("vendor-js", "build-js"), done => {
+    return gulp
+      .src([build + "vendor-bundle.js", build + "main.js"])
+      .pipe(sourcemaps.init())
+      .pipe(concat("bundle.js"))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(js_temp));
+    done();
+  })
+);
+
+// uglifyJS
+gulp.task(
+  "compress-js",
+  gulp.series("bundle-js", function(cb) {
+    pump([gulp.src(temp + "**/*.js"), uglify(), gulp.dest(temp)], cb);
+  })
+);
+
+// images optimising
+gulp.task("optimise-img", () => {
+  return gulp
+    .src(img_src + "*.+(png|jpg|jpeg|gif|svg)")
+    .pipe(
+      cache(
+        imagemin({
+          interlaced: true
+        })
+      )
+    )
+    .pipe(gulp.dest(assets));
+});
+
+// html files build
+gulp.task(
+  "build-html",
+  gulp.series(function(done) {
+    return gulp.src(html_src).pipe(gulp.dest(dist));
+    done();
+  })
+);
+
+// build and minify
+gulp.task(
+  "build-compress",
+  gulp.parallel("build-html", "build-sass", "compress-js", "optimise-img")
+);
+
+// build files
+gulp.task(
+  "build-all",
+  gulp.parallel("build-html", "build-sass", "bundle-js", "optimise-img")
+);
+
+// clean previous build
+gulp.task("clean", function(done) {
+  return del([dist]);
+  done();
+});
+
+// clean html files for update
+gulp.task("clean-html", done => {
+  del.sync([dist + "/*.html"]);
+  done();
+});
+
+// delete assets except js and css files
+gulp.task("delete-assets", () => {
+  return del([assets + "/*", "!./dist/assets/rev-manifest.json"]);
+});
+
+// watching scss/js/html files
+gulp.task("watch", function(done) {
+  gulp.watch(sass_files, gulp.series("live-reload"));
+  gulp.watch("./src/*.js", gulp.series("live-reload"));
+  gulp.watch(html_src).on(
+    "change",
+    gulp.series(
+      "clean-html",
+      "build-html",
+      "update",
+      "delete-assets",
+      "optimise-img",
+      done => {
+        browserSync.reload();
+        done();
+      }
+    )
+  );
+  done();
+});
+
+// Static Server
+gulp.task(
+  "serve",
+  gulp.parallel("watch", () => {
+    browserSync.init({
+      server: {
+        baseDir: "./dist/"
+      }
+    });
+  })
+);
+
+// live reloading
+gulp.task(
+  "live-reload",
+  gulp.series("clean", "build-all", "update", function(done) {
+    browserSync.reload();
+    done();
+  })
+);
+
+// build and serve
+gulp.task("default", gulp.series("clean", "build-all", "update", "serve"));
+
+// build for production
+gulp.task("build", gulp.series("clean", "build-compress", "update"));
